114d4b8a649927ac24053dd09f7a9e4b5e901993
Justin Valentin <Justinvalentine88@gmail.com>
2017-11-14 10:56:43 -0500

    this

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :eshops
+
   get 'charges/new'
 
   get 'charges/create'
 
-  resources :eshops
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'eshops#index'
+
+  resources :charges, only: [:new, :create]
+  resources :posts
+  root to: 'posts#index'
 end
